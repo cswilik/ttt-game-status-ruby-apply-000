12ef8533ff8e16912356ca372dbf4bc3ce96394d
Chelsey <cswilik@gmail.com>
2020-09-27 23:11:36 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,14 +6,14 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [ #parent array that contains 8 children
 #arrays w 3 elements each
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
+  [0,1,2], # top row 
+  [3,4,5], #middle row 
+  [6,7,8], #bottom row 
+  [0,3,6], #left column
+  [1,4,7], # middle column
+  [2,5,8], # right column 
+  [0,4,8], # left to right diagonal 
+  [2,4,6] # right to left diagonal 
   ]
   
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
